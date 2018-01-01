36917c4e9062ca46955998a0c460f072e3f5ac8e
elliotec <elliotecweb@gmail.com>
2017-12-31 21:24:42 -0700

    add package json and some deps

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
-const BASE_CRYPTONATOR_API_URI = 'https://api.cryptonator.com/api'
-const CURRENCIES_URI = `${BASE_API_URI}/currencies`
-const TICKER_URI = `${BASE_API_URI}/ticker`
-const FULL_TICKER_URI = `${BASE_API_URI}/full`
+// const BASE_CRYPTONATOR_API_URL = 'https://api.cryptonator.com/api'
+// const BASE_BINANCE_API_URL = 'https://api.binance.com/api/v1'
+// const BINANCE_ALL_PRICES_URL = `${BASE_BINANCE_API_URL}/ticker/allPrices`
+// const CURRENCIES_URL = `${BASE_CRYPTONATOR_API_URL}/currencies`
+// const TICKER_URL = `${BASE_CRYPTONATOR_API_URL}/ticker`
+// const FULL_TICKER_URL = `${BASE_CRYPTONATOR_API_URL}/full`
 
-function fetchAllCurrencies() {
-  fetch(CURRENCIES_URI)
-    .then(response => response.json())
-    .then(data => fetchTickerData(data))
-    .catch(error => error)
-}
+// function fetchAllCurrencies() {
+//   fetch(BINANCE_ALL_PRICES_URL)
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => error)
+// }
 
-function fetchTickerData(data) {
-  const currencies = data.rows
-  currencies.map((currency) => {
-    const currencyCode = currency.code.toLowerCase()
-    const currencyUri = `${FULL_TICKER_URI}/${currencyCode}-usd`
-    fetch(currencyUri)
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => error)
-  })
-}
+// function fetchTickerData(data) {
+//   const currencies = data.rows
+//   currencies.map((currency) => {
+//     const currencyCode = currency.code.toLowerCase()
+//     const currencyUri = `${FULL_TICKER_URL}/${currencyCode}-usd`
+//     fetch(currencyUri)
+//       .then(response => response.json())
+//       .then(data => console.log(data))
+//       .catch(error => error)
+//   })
+// }
 
-fetchAllCurrencies()
+// fetchAllCurrencies()
