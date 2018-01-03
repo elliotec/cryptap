e356f72cb4a1c60dcfd9509fe740fb0b7b2ce649
elliotec <elliotecweb@gmail.com>
2018-01-02 19:46:50 -0700

    add es6 template engine

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello bab!'))
+app.get('/', (req, res) => res.send('Hello!'))
 
 app.listen(3000, () => console.log('listening on port 3000, bich'))
