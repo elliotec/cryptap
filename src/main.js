ae5b1081ec31f813742c7c920dca197b0b70d005
elliotec <elliotecweb@gmail.com>
2018-01-02 23:59:49 -0700

    boom get onKline working and printing to console/template. need to emit to browser with websockets

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import express from 'express'
 import es6Renderer from 'express-es6-template-engine'
-// import binance from 'binance'
+import binanceApi from 'binance'
+
+const state = {}
+const binance = new binanceApi.BinanceWS()
+
+binance.onKline('BNBBTC', '1m', (data) => {
+  console.log(data.kline.volume)
+  state.kline = data.kline
+})
+
 const app = express()
 
 app.engine('html', es6Renderer)
 app.set('views', 'views')
 app.set('view engine', 'html')
 
-app.get('/', (req, res) => res.render('index', {locals: {title: 'omg'}}))
+app.get('/', (req, res) => res.render('index', {locals: {title: state.kline.volume}}))
 
 app.listen(3000, () => console.log('listening on port 3000, bich'))
