b0714318cacf41b62b8afdc3428aa2039dad9639
elliotec <elliotecweb@gmail.com>
2018-01-02 23:16:26 -0700

    add es6 template engine and render es6

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import express from 'express'
+import es6Renderer from 'express-es6-template-engine'
 // import binance from 'binance'
-
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello!'))
+app.engine('html', es6Renderer)
+app.set('views', 'views')
+app.set('view engine', 'html')
+
+app.get('/', (req, res) => res.render('index', {locals: {title: 'omg'}}))
 
 app.listen(3000, () => console.log('listening on port 3000, bich'))
