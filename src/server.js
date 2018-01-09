3880de0ca0c3bd1ea5f70063ee8d5dd2bb3b58a9
elliotec <elliotecweb@gmail.com>
2018-01-09 00:18:41 -0700

    get top 100 coins and log each's onKline

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ import es6Renderer from 'express-es6-template-engine'
 import binanceApi from 'binance'
 import http from 'http'
 import socketIo from 'socket.io'
+import fetch from 'node-fetch'
 
 const state = {}
 const binance = new binanceApi.BinanceWS()
 const app = express()
 const server = http.createServer(app)
 const io = socketIo(server)
+const coinMarketCapApi = 'https://api.coinmarketcap.com/v1/ticker/'
 
 app.engine('html', es6Renderer)
 app.set('views', 'views')
@@ -20,13 +22,36 @@ app.get('/', (req, res) => res.render('index', {
 }))
 
 io.on('connection', (socket) => {
+  fetch(coinMarketCapApi)
+    .then((res) => res.json())
+    .then((json) => getAllSymbols(json))
+
+  function getAllSymbols (data) {
+    console.log(data.map((coin) => {
+      return binance.onKline(`${coin.symbol}BTC`, '5m', (data) => {
+        state.kline = data.kline
+        const open = state.kline.open
+        const close = state.kline.close
+        const change = (close - open) / open * 100
+        const roundedChange = Number.parseFloat(change).toPrecision(4)
+        console.log(`${roundedChange}%`)
+        state.percentChange = roundedChange
+
+        socket.emit('broadcast', { state })
+      })
+    }))
+    // return data.map((coin) => {
+    //   return coin.symbol
+    // })
+  }
+
   binance.onKline('ETHBTC', '5m', (data) => {
     state.kline = data.kline
     const open = state.kline.open
     const close = state.kline.close
     const change = (close - open) / open * 100
     const roundedChange = Number.parseFloat(change).toPrecision(4)
-    console.log(`${roundedChange}%`)
+    // console.log(`${roundedChange}%`)
     state.percentChange = roundedChange
 
     socket.emit('broadcast', { state })
